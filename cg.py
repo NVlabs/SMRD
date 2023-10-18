1cc544ad4b395cf9370d02a69e0f653f7352b945
Batu Ozturkler <47992169+batuozt@users.noreply.github.com>
2023-10-17 21:44:12 -0700

    Update cg.py

--- cg.py
+++ cg.py
@@ -1,3 +1,40 @@
+# MIT License
+"""
+MIT License
+
+Copyright (c) 2021 The University of Texas Computational Sensing and Imaging Lab
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+"""
+------------------------------------------------------------------------------
+Copyright (c) 2022-2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+
+This work is made available under the Nvidia Source Code License.
+To view a copy of this license, visit
+https://github.com/batuozt/SMRD/blob/master/LICENSE.md
+
+Written by Batu Ozturkler
+------------------------------------------------------------------------------
+"""
+
 import torch
 from torch import nn
 
